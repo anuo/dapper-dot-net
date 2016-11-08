0d090bd12d892950d3cab1f75ff251b914a0efbf
anuo <1587726402@qq.com>
2016-11-08 14:12:40 +0800

    s

---
+++ Dapper.Study/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper.Study
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var con = SQLServerHelper.GetConnection())
+            {
+                string query = "SELECT* FROM dbo.UserAccount WHERE Code =@Code";
+                con.Query<dynamic>(query, new { @Code = "admin16" }).ToList();
+
+            }
+        }
+    }
+}
---
+++ Dapper.Study/SQLServerHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Dapper.Study
+{
+    public class SQLServerHelper
+    {
+        public static SqlConnection sqlServerCon;
+        public static string conStr;
+        public static SqlConnection GetConnection()
+        {
+
+            if (string.IsNullOrEmpty(conStr))
+            {
+                conStr = @"Data Source=192.168.0.3\ydlmserver;Initial Catalog=YDL;User Id=sa;Password=ydl_151001;";
+            }
+
+            sqlServerCon = new SqlConnection(conStr);
+            sqlServerCon.Open();
+ 
+            return sqlServerCon;
+
+
+      
+        }
+    }
+}
